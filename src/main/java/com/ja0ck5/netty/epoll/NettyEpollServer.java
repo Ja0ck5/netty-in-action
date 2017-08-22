f3cff73b4ba9db8213de97203fd00adf5b9df47b
ja0ck5 <975117619@qq.com>
2017-08-22 23:37:32 +0800

    netty epoll

--- src/main/java/com/ja0ck5/netty/epoll/NettyEpollServer.java
+++ src/main/java/com/ja0ck5/netty/epoll/NettyEpollServer.java
@@ -1,9 +1,11 @@
-package com.ja0ck5.netty.noio;
+package com.ja0ck5.netty.epoll;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
+import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.epoll.EpollServerSocketChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -12,17 +14,22 @@ import io.netty.util.CharsetUtil;
 import java.net.InetSocketAddress;
 
 /**
+ * 由 JNI 驱动的 epoll() 和非阻塞 IO。这个传输支持只有在 Linux 上可用的多重特性,
+ * 如 SO_REUSEPORT, 比 NIO 传输更快，而且是完全非阻塞的。
+ * 如果 应用程序是运行在 Linux 上，那么请利用这个版本的传输，在高速在的情况下，性能要由于 JDK 的 NIO 实现
+ * <p>
+ * <p>
  * Created by Ja0ck5 on 2017/8/22.
  */
-public class NettyNioServer {
+public class NettyEpollServer {
 
     public void server(int port) {
         final ByteBuf byteBuf = Unpooled.unreleasableBuffer(Unpooled.copiedBuffer("Hi!\r\n", CharsetUtil.UTF_8));
-        NioEventLoopGroup nioGroup = new NioEventLoopGroup();
+        EpollEventLoopGroup epollGroup = new EpollEventLoopGroup();
         try {
             ServerBootstrap b = new ServerBootstrap();
-            b.group(nioGroup)//
-                    .channel(NioServerSocketChannel.class)// 使用 NioServerSocketChannel 以允许非阻塞模式
+            b.group(epollGroup)//
+                    .channel(EpollServerSocketChannel.class)// 使用 NioServerSocketChannel 以允许非阻塞模式
                     .localAddress(new InetSocketAddress(port))
                     // 指定 ChannelInitializer ，对于每个已接受的连接都调用它
                     .childHandler(new ChannelInitializer<SocketChannel>() {
@@ -33,16 +40,7 @@ public class NettyNioServer {
                                 public void channelActive(ChannelHandlerContext ctx) throws Exception {
                                     // 将消息写到客户端，并添加 ChannelFutureListener 以便消息一被写完就关闭连接
                                     ctx.writeAndFlush(byteBuf.duplicate())
-                                            .addListener((ChannelFutureListener) channelFuture -> {
-                                                if (channelFuture.isSuccess()) {
-                                                    System.out.println("Write successful");
-                                                } else {
-                                                    System.err.println("Write error");
-                                                    channelFuture.cause().printStackTrace();
-
-                                                }
-                                            })
-                                            /*.addListener(ChannelFutureListener.CLOSE)*/;
+                                            .addListener(ChannelFutureListener.CLOSE);
                                 }
                             });
                             ChannelFuture f = b.bind().sync();
@@ -51,7 +49,7 @@ public class NettyNioServer {
                     });
 
         } finally {
-            nioGroup.shutdownGracefully();
+            epollGroup.shutdownGracefully();
         }
 
     }
