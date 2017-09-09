084cc11e57fd348726d59ba508dbb957e0c5b6ae
ja0ck5 <975117619@qq.com>
2017-09-09 23:11:22 +0800

    client

---
+++ src/main/java/com/ja0ck5/netty/bootstrap/BootstrapClient.java
@@ -0,0 +1,41 @@
+package com.ja0ck5.netty.bootstrap;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.net.InetSocketAddress;
+
+/**
+ * Created by Ja0ck5 on 2017/9/9.
+ */
+public class BootstrapClient {
+
+    public static void main(String[] args) {
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new SimpleChannelInboundHandler<ByteBuf>() {
+                    @Override
+                    protected void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) throws Exception {
+                        System.out.println("Received data");
+                    }
+                });
+        ChannelFuture f = bootstrap.connect(new InetSocketAddress("www.manning.com", 80));
+        f.addListener((ChannelFutureListener) channelFuture -> {
+            if (channelFuture.isSuccess()) {
+                System.out.println("Connection established");
+            } else {
+                System.err.println("Connection attempt failed");
+                channelFuture.cause().printStackTrace();
+            }
+        });
+    }
+
+}
